91262d8c7e9853f38b94aee3dd1d4a732e7e876a
MarynaDanilkina <Danilkinamari99@gmail.com>
2022-11-08 22:40:21 +0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import Header from 'components/header/header';
+import Welcome from 'pages/welcome/welcome';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/log-in" element={<Welcome />} />
+        <Route path="/sign-up" element={<Welcome />} />
+        <Route path="/profile" element={<Welcome />} />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/board/:id" element={<Welcome />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.sass';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <div className="header__logo">
+          <Link to="/welcome">
+            <img src="/logo.png" className="header__logo" />
+          </Link>
+          <h1>team board</h1>
+        </div>
+        <nav className="nav">
+          <div className="nav__lang">RU</div>
+          <div className="nav__log-in">
+            <Link to="/log-in">Войти</Link>
+          </div>
+          <div className="nav__sign-up-home">
+            <Link to="/sign-up">Зарегистрироваться</Link>
+          </div>
+
+          <div className="nav__item">
+            <Link to="/">+ Новая доска</Link>
+          </div>
+          <div className="nav__item">
+            <Link to="/profile">Профиль</Link>
+          </div>
+          <div className="nav__log-in">
+            <p>Выйти</p>
+          </div>
+          <div className="nav__sign-up-home">
+            <Link to="/">На главную</Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+export default Header;
