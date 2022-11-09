c01e414ba5b62804cfeab417b2b652b4f0e1602a
Khalid <shkhalid95@mail.ru>
2022-11-10 00:33:07 +0300

    refactor: refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
-import ConfirmDialogWindow from 'components/confirmDialogWindow';
-import AddColumnModal from 'components/addColumnModal';
-import EditBoardOrAddBoardOrAddTaskModal from 'components/editBoardOrAddBoardOrAddTaskModal';
-import EditTaskModal from 'components/editTaskModal/EditTaskModal';
+import React from 'react';
 
 function App() {
-  const titleRef = useRef<HTMLInputElement>(null!);
-  const selectRef = useRef<HTMLSelectElement>(null!);
-  const [titleError, setTitleError] = useState(false);
-
-  const onOk = () => {
-    console.log(titleRef.current?.value);
-    console.log(selectRef.current?.value);
-    titleRef.current?.value === '' && setTitleError(true);
-  };
-
-  return (
-    /*
-    <EditTaskModal
-      title="Edit task"
-      titleRef={titleRef}
-      selectRef={selectRef}
-      defaultValue="some"
-      selectedValue="Zidan"
-      selectValues={['Zidan', 'Messi', 'Rooney']}
-      isModalOpen={true}
-      titleError={titleError}
-      onClose={() => {}}
-      onOk={onOk}
-    />
-    */
-    /*
-    <EditBoardOrAddBoardOrAddTaskModal
-      title="Add Task"
-      titleError={false}
-      descriptionError={false}
-      titleInputID="task-title-input"
-      descreptionInputID="task-descreption-input"
-      titleLabel="Title of the task"
-      descreptionLabel="Descreption of the task"
-      isModalOpen={true}
-      onClose={() => {}}
-      onOk={() => {}}
-    />
-    */
-    /*<EditBoardOrAddBoardOrAddTaskModal
-      title="Add Board"
-      titleError={false}
-      descriptionError={false}
-      titleInputID="board-title-input"
-      descreptionInputID="board-descreption-input"
-      titleLabel="Title of the board"
-      descreptionLabel="Descreption of the board"
-      isModalOpen={true}
-      onClose={() => {}}
-      onOk={() => {}}
-    />*/
-    // <AddColumnModal titleError={false} isModalOpen={true} onClose={() => {}} onOk={() => {}} />
-    // <ConfirmDialogWindow title="Add Board" isModalOpen={true} onClose={() => {}} onOk={() => {}} />
-  );
+  return <></>;
 }
 
 export default App;
