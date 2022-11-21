fd57e0ae5dbfb74c18e6bfc9acf09153f19954b7
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-11-21 17:43:43 +0300

    feat: add task modal

--- src/components/button_addTask/addTask.tsx
+++ src/components/button_addTask/addTask.tsx
@@ -1,27 +1,55 @@
 import { Column } from 'api/contracts';
 import { fetchCreateTask } from 'api/tasksApi';
 import { useAppDispatch, useAppSelector } from 'interface/interface';
-import React from 'react';
-const title = 'Новая задача2';
-const description = 'описание задачи';
+import Modal from '../editBoardOrAddBoardOrAddTaskDialogWindow';
+import React, { useRef, useState } from 'react';
+
 const userId = 'dd398f23-1324-4e5c-95f7-0dd193b5e89f';
 const token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIzMjQ4ODM3OS1hMDhjLTQ3YjMtOWNkNi01NjU5Y2JiNzg2NTYiLCJsb2dpbiI6InVzZXIwMDEyMiIsImlhdCI6MTY2ODE2NjcyN30.8ywrrjkBcaLGETqLwbAqwBojiGkbS2PnIS9QtotEUO8';
-export type IBoardProps = {
-  board: Column;
-};
-const AddTask = ({ board }: IBoardProps) => {
+
+const AddTask = ({ board }: { board: Column }) => {
   const dispatch = useAppDispatch();
   const { boards } = useAppSelector((state) => state.boards);
-  const boardId = boards.id;
-  const columnId = board.id;
+
+  const [modal, setModal] = useState(false);
+  const inputRefTitle = useRef<HTMLInputElement>(null!);
+  const inputRefDescription = useRef<HTMLSelectElement>(null!);
+  const getRefs = () => ({ inputRefTitle, inputRefDescription });
+
   function addTask() {
-    dispatch(fetchCreateTask({ title, description, userId, token, boardId, columnId }));
+    dispatch(
+      fetchCreateTask({
+        title: inputRefTitle.current.value,
+        description: inputRefDescription.current.value,
+        userId,
+        token,
+        boardId: boards.id,
+        columnId: board.id,
+      })
+    );
+    setModal(false);
   }
+
   return (
-    <div className="add__column-tasks" onClick={() => addTask()}>
-      <p>+ Добавить задачу</p>
-    </div>
+    <>
+      <div className="add__column-tasks" onClick={() => setModal(true)}>
+        <p>+ Добавить задачу</p>
+      </div>
+      <Modal
+        titleError={true}
+        titleLabel="Название: "
+        descriptionLabel="Описание: "
+        titleInputID="1"
+        descriptionInputID="2"
+        onFocus={() => {}}
+        getRefs={getRefs}
+        onOk={() => addTask()}
+        onClose={() => setModal(false)}
+        isModalOpen={modal}
+      />
+    </>
   );
 };
+
 export default AddTask;
