a00706996830a19828f975c61363fb551d010b9e
MarynaDanilkina <Danilkinamari99@gmail.com>
2022-11-30 23:53:54 +0300

    feat: add scrollbar

--- src/components/colomn/colomn.tsx
+++ src/components/colomn/colomn.tsx
@@ -27,20 +27,22 @@ const Colomn = ({ board, index, ColomnRef, draggableProps, dragHandleProps }: IB
             {...providedTasks.droppableProps}
           >
             <h3>{board.title}</h3>
-            {board.tasks.map((task, index) => (
-              <Draggable key={task.id} draggableId={task.id} index={index}>
-                {(providedTask) => (
-                  <div
-                    ref={providedTask.innerRef}
-                    {...providedTask.draggableProps}
-                    {...providedTask.dragHandleProps}
-                  >
-                    <TaskCompon task={task} key={task.id} />
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {providedTasks.placeholder}
+            <div className="task-Column__container">
+              {board.tasks.map((task, index) => (
+                <Draggable key={task.id} draggableId={task.id} index={index}>
+                  {(providedTask) => (
+                    <div
+                      ref={providedTask.innerRef}
+                      {...providedTask.draggableProps}
+                      {...providedTask.dragHandleProps}
+                    >
+                      <TaskCompon task={task} key={task.id} />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {providedTasks.placeholder}
+            </div>
             <AddTask board={board} />
           </div>
         )}
