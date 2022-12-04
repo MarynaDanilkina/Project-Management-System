0f4e86bc1d983335a5c224b130e8d4bee46c2294
Ryasnyanskaya Kseniya <ryasnyanskaya_kseniya@inbox.ru>
2022-12-04 09:56:25 +0300

    feat: add task update

--- src/components/colomn/colomn.tsx
+++ src/components/colomn/colomn.tsx
@@ -1,18 +1,53 @@
 import { Column } from 'api/contracts';
 import AddTask from 'components/button_addTask/addTask';
 import TaskCompon from 'components/task/task';
-import React from 'react';
+import ConfirmDialogWindow from 'components/confirmDialogWindow';
+import { useAppSelector } from 'interface/interface';
+import { selectToken, selectUser } from 'toolkitRedux/userSlice/userSlice';
+import { deleteColumn } from 'api/columnsApi';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Colomn = ({ board }: { board: Column }) => {
+  const token = useAppSelector(selectToken);
+  const user = useAppSelector(selectUser);
+  const { boards } = useAppSelector((state) => state.boards);
+  const { t } = useTranslation();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const toggleIsModalOpen = () => setIsModalOpen(!isModalOpen);
+
+  const onModalOk = () => {
+    if (token && user?.userId) {
+      deleteColumn(token, boards.id, board.id);
+      toggleIsModalOpen(); //! Нужен перерендер
+    }
+  };
+
+  // редактирование задачи
+  // updateTask
+
+  // редактирование столбца
+  // updateColumn
+
   return (
     <div className="Board__column">
       <h3>{board.title}</h3>
       <div>
         {board.tasks.map((task) => (
-          <TaskCompon task={task} key={task.id} />
+          <TaskCompon task={task} board={board} key={task.id} />
         ))}
       </div>
       <AddTask board={board} />
+      <div onClick={() => setIsModalOpen(true)}>
+        <p>Будет урна</p>
+      </div>
+      <ConfirmDialogWindow
+        title={`${t('delete_board_warning')} "${board.title}"?`}
+        onOk={onModalOk}
+        onClose={toggleIsModalOpen}
+        isModalOpen={isModalOpen}
+      />
     </div>
   );
 };
--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,13 +1,87 @@
 import { Task } from 'api/contracts';
-import React from 'react';
+import { Column } from 'api/contracts';
+import React, { useState, useRef } from 'react';
+import ConfirmDialogWindow from 'components/confirmDialogWindow';
+import { useAppSelector } from 'interface/interface';
+import { selectToken, selectUser } from 'toolkitRedux/userSlice/userSlice';
+import ModalForEditTask from '../editBoardOrAddBoardOrAddTaskDialogWindow';
+import { deleteTask, updateTask } from 'api/tasksApi';
+import { useTranslation } from 'react-i18next';
+
 export interface ITask {
   task: Task;
+  board: Column;
 }
-const TaskCompon = ({ task }: ITask) => {
+
+const TaskCompon = ({ task, board }: ITask) => {
+  const token = useAppSelector(selectToken);
+  const user = useAppSelector(selectUser);
+  const { boards } = useAppSelector((state) => state.boards);
+  const { t } = useTranslation();
+
+  const inputRefTitle = useRef<HTMLInputElement>(null!);
+  const inputRefDescription = useRef<HTMLSelectElement>(null!);
+  const getRefs = () => ({ inputRefTitle, inputRefDescription });
+
+  const [deleteModalActive, setDeleteModalActive] = useState(false);
+  const [updateModalActive, setUpdateModalActive] = useState(false);
+  const toggleDeleteModalOpen = () => setDeleteModalActive(!deleteModalActive);
+  const toggleUpdateModalOpen = () => setUpdateModalActive(!updateModalActive);
+
+  const onDeleteModalOk = () => {
+    if (token && user?.userId) {
+      deleteTask(token, boards.id, board.id, task.id);
+      toggleDeleteModalOpen(); //! Нужен перерендер
+    }
+  };
+
+  const onUpdateModalOk = () => {
+    if (token && user?.userId) {
+      updateTask(
+        inputRefTitle.current.value,
+        task.order,
+        inputRefDescription.current.value,
+        token,
+        boards.id,
+        board.id,
+        task.id,
+        user.userId
+      );
+      toggleUpdateModalOpen(); //! Нужен перерендер
+    }
+  };
+
   return (
     <div className="Board__column-items" draggable={true}>
-      <div className="Board__column-item">{task.title}</div>
+      <div className="Board__column-item">
+        {task.title}
+        <div onClick={() => setDeleteModalActive(true)}>
+          <p>Урна для задачи</p>
+        </div>
+        <div onClick={() => setUpdateModalActive(true)}>
+          <p>Значок редактирования</p>
+        </div>
+      </div>
+      <ConfirmDialogWindow
+        title={`${t('delete_task_warning')} "${task.title}"?`}
+        onOk={onDeleteModalOk}
+        onClose={toggleDeleteModalOpen}
+        isModalOpen={deleteModalActive}
+      />
+      <ModalForEditTask
+        titleError={true}
+        titleLabel={t('title')}
+        descriptionLabel={t('description')}
+        titleInputID="1"
+        descriptionInputID="2"
+        onFocus={() => {}}
+        getRefs={getRefs}
+        onOk={() => onUpdateModalOk()}
+        onClose={() => setUpdateModalActive(false)}
+        isModalOpen={updateModalActive}
+      />
     </div>
   );
 };
+
 export default TaskCompon;
